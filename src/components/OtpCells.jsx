ae6ba4bfa0bad06146ccee2082aa649564c65a5d
ankugargdev619 <137602411+ankugargdev619@users.noreply.github.com>
2024-11-07 22:07:57 +0530

    Add files via upload

--- src/components/OtpCells.jsx
+++ src/components/OtpCells.jsx
@@ -41,7 +41,7 @@ export const OtpCells = ({cellCount,isDisabled,setIsDisabled}) => {
                         const pattern = /^[0-9]$/;
                         const allowed = ["Backspace", "ArrowRight","ArrowLeft","Delete"];
 
-                        if(!(pattern.test(e.key) || allowed.includes(e.key)) ){
+                        if(!(pattern.test(e.key) || allowed.includes(e.key) || inputRef.current[i].value != "") ){
                             e.preventDefault();
                         }
 
